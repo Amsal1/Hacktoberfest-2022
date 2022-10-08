383f1e48edd5c5c12732c610bea0e96a621ceca9
Khubaib Ahmad <84956431+khubaibkm@users.noreply.github.com>
2022-10-08 21:33:14 +0530

    Create Hello.cpp

---
+++ Programs/Hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Hello World, This is Khubaib Ahmad from Integral University"<<endl;
+  cout<<"This is my first time in Hacktoberfest #2k22";
+  return 0;
+}
